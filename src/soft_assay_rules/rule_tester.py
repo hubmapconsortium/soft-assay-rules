11d995d18f201a0630081ce691d22e8af5983f51
Joel Welling <welling@psc.edu>
2024-01-30 16:40:19 -0500

    Add json support to rule_tester

--- src/soft_assay_rules/rule_tester.py
+++ src/soft_assay_rules/rule_tester.py
@@ -7,7 +7,10 @@ from os.path import isdir
 from pprint import pprint
 import pandas as pd
 
+from local_rule_tester import print_rslt
+
 AUTH_TOK = 'some_token'
+AUTH_TOK = 'AgbMb5GX7POdjrzE7vvmpxjBVpeoWxgVba5jnvzPbaOrOkM1P8C5CddlYQKKJQrY3P9dM7p9833V23UkGNnNpfz9r0'
 
 TEST_BASE_URL = 'http://localhost:5000/'
 
@@ -20,18 +23,44 @@ def main() -> None:
             print(f"Skipping directory {argfile}")
             continue
         print(f"Reading {argfile}")
-        arg_df = None
         if argfile.endswith('.tsv'):
             arg_df = pd.read_csv(argfile, sep='\t')
-        else:
-            raise RuntimeError(f"Arg file {argfile} is of an"
-                               " unrecognized type")
-        if len(arg_df.columns) == 1 and 'uuid' in arg_df.columns:
-            for idx, row in arg_df.iterrows():
-                print(f"{row['uuid']} ->")
-                try:
-                    rply = requests.get(
-                        TEST_BASE_URL + 'assaytype' + '/' + row['uuid'],
+            if len(arg_df.columns) == 1 and 'uuid' in arg_df.columns:
+                for idx, row in arg_df.iterrows():
+                    print(f"{row['uuid']} ->")
+                    try:
+                        rply = requests.get(
+                            TEST_BASE_URL + 'assaytype' + '/metadata/' + row['uuid'],
+                            headers={
+                                'Authorization': 'Bearer ' + AUTH_TOK,
+                                'content-type': 'application/json'
+                            }
+                        )
+                        rply.raise_for_status()
+                        print(f"-> json metadata: {json.dumps(rply.json())} ->")
+
+                        rply = requests.get(
+                            TEST_BASE_URL + 'assaytype' + '/' + row['uuid'],
+                            headers={
+                                'Authorization': 'Bearer ' + AUTH_TOK,
+                                'content-type': 'application/json'
+                            }
+                        )
+                        rply.raise_for_status()
+                        rslt = rply.json()
+                        pprint(rslt)
+                        if not rslt:
+                            print("NOT MAPPED!")
+                    except requests.exceptions.HTTPError as excp:
+                        print(f"ERROR: {excp}")
+            else:
+                #print(arg_df)
+                for idx, row in arg_df.iterrows():
+                    payload = {col: row[col] for col in arg_df.columns}
+                    #pprint(payload)
+                    rply = requests.post(
+                        TEST_BASE_URL + 'assaytype',
+                        data=json.dumps(payload),
                         headers={
                             'Authorization': 'Bearer ' + AUTH_TOK,
                             'content-type': 'application/json'
@@ -39,16 +68,16 @@ def main() -> None:
                     )
                     rply.raise_for_status()
                     rslt = rply.json()
+                    print(f"{argfile} {idx} ->")
                     pprint(rslt)
                     if not rslt:
-                        print("NOT MAPPED!")
-                except requests.exceptions.HTTPError as excp:
-                    print(f"ERROR: {excp}")
-        else:
-            #print(arg_df)
-            for idx, row in arg_df.iterrows():
-                payload = {col: row[col] for col in arg_df.columns}
-                #pprint(payload)
+                        print("Payload follows")
+                        pprint(payload)
+        elif argfile.endswith('.json'):
+            try:
+                with open(argfile) as jsonfile:
+                    payload = json.load(jsonfile)
+                print(f"{argfile} -> {payload} ->")
                 rply = requests.post(
                     TEST_BASE_URL + 'assaytype',
                     data=json.dumps(payload),
@@ -59,11 +88,14 @@ def main() -> None:
                 )
                 rply.raise_for_status()
                 rslt = rply.json()
-                print(f"{argfile} {idx} ->")
                 pprint(rslt)
                 if not rslt:
-                    print("Payload follows")
-                    pprint(payload)
+                    print("NOT MAPPED!")
+            except requests.exceptions.HTTPError as excp:
+                print(f"ERROR: {excp}")
+        else:
+            raise RuntimeError(f"Arg file {argfile} is of an"
+                               " unrecognized type")
     print('done')
 
 if __name__ == '__main__':
