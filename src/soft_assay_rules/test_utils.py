87a52bc753427aa6e69e0fc5312ae08b7f583fef
Joel Welling <welling@psc.edu>
2024-09-10 13:34:02 -0400

    Move print_rslt to a separate module

---
+++ src/soft_assay_rules/test_utils.py
@@ -0,0 +1,21 @@
+import json
+from pprint import pprint
+
+def print_rslt(argfile, idx, payload, rslt, show_payload=False):
+    """
+    This just summarizes the results of a test case to stdout
+    """
+    if show_payload:
+        print(f"{argfile} {idx if idx else ''} -> json metadata: {json.dumps(payload)} ->")
+        pprint(rslt)
+        if not rslt:
+            print("NOT MAPPED!")
+    else:
+        print(f"{argfile} {idx if idx else ''} ->")
+        pprint(rslt)
+        if not rslt:
+            print("NOT MAPPED!")
+            print("Payload follows")
+            pprint(payload)
+
+
