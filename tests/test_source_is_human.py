963eaaf56a2b076e7d1aa2e13652466d6c5190f5
Joel Welling <welling@psc.edu>
2024-09-12 17:08:27 -0400

    test_source_is_human restructure and capture logs

--- tests/test_source_is_human.py
+++ tests/test_source_is_human.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import re
 import json
+import logging
 
 import pandas as pd
 import pytest
@@ -9,12 +10,18 @@ from pprint import pprint
 from local_rule_tester import wrapped_lookup_json
 from source_is_human import source_is_human
 
+logging.basicConfig(encoding="utf-8", level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
 
-@pytest.mark.parametrize(('uuid', 'expected_value'), (
-    ("c019a1cd35aab4d2b4a6ff221e92aaab", True),
-    ("SNT658.QHTS.972", True),
-    ("SNT594.FZCM.747", False)
+@pytest.mark.parametrize(('uuid_list', 'expected_value'), (
+    (["c019a1cd35aab4d2b4a6ff221e92aaab"], True),
+    (["SNT658.QHTS.972"], True),
+    (["SNT594.FZCM.747"], False),
+    (["SNT658.QHTS.972", "SNT594.FZCM.747"], True),
+    (["SNT594.FZCM.747", "SNT658.QHTS.972"], True),
+    (["SYNTH1"], True),
 ))
-def test_sample_is_human(uuid, expected_value, tmp_path):
-    val = source_is_human([uuid], wrapped_lookup_json)
-    assert val == expected_value
+def test_source_is_human(uuid_list, expected_value, tmp_path, caplog):
+    with caplog.at_level(logging.DEBUG):
+        val = source_is_human(uuid_list, wrapped_lookup_json)
+        assert val == expected_value
